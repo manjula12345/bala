983c67e6555c4e233697acada1dfdfd05ac574ee
manjula12345 <30342659+manjula12345@users.noreply.github.com>
2017-08-12 16:38:04 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ 
+void main()
+{
+   int *pointer1, *pointer2;
+   int number;
+ 
+   printf("\nEnter two numbers : ");
+   scanf("%d %d", pointr1, pointer2);
+ 
+   number = *pointerr1 + *pointer2;
+ 
+   printf("Sum = %d", number);
+   
+}
