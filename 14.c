4bfb4ffa92d5403a046cc943b70f4d99d4c2f37c
manjula12345 <30342659+manjula12345@users.noreply.github.com>
2017-08-12 16:57:47 +0530

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,10 @@
+elm_st_and_2nd[12][30].edges :
+ elm_st_and_1st[12][35] # 6 minute walk to the next intersection
+   time = 6 minutes
+   transport = foot
+ main_st_and_25th[1][15] # 30 minute bus ride
+   time = 30 minutes
+   transport = bus
+ elm_st_and_1st[12][36] # stay in one place for one minute
+   time = 2 minute
+   transport = stand still
